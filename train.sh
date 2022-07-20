e8399b6a35e437a2d79754d3d537d2cc2471715f
xinliu20 <109497799+xinliu20@users.noreply.github.com>
2022-07-21 04:39:05 +0800

    Initial commit

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 python main.py \
-  --dir_data /home/liux/datasets/nyudepth_hdf5 \
+  --dir_data /path/to/your/dataset \
   --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --split_json ./data/nyu.json --num_sample 500 \
